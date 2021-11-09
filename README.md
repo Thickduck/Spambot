00f5f8979ddc3e95cd5bcea647fdbc1a4c88486d
Thickduck <89892224+Thickduck@users.noreply.github.com>
2021-11-09 19:57:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple spambot using pyautogui. 
+
+NOT TO BE MISUSED!
+
+PR's are appreciated 
+peace
---
+++ spambot.py
@@ -0,0 +1,7 @@
+import pyautogui, time
+
+time.sleep (5.0)
+
+while True:
+    pyautogui.typewrite("<your text here>")
+    pyautogui.press("enter")
